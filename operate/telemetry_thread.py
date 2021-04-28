0593df03466cdcb4c78c8bb1ffa45cee819935cb
huyfr <61008601+huyfr@users.noreply.github.com>
2021-04-28 08:41:01 +0700

    read real telemetry from stm32

--- operate/telemetry_thread.py
+++ operate/telemetry_thread.py
@@ -8,7 +8,7 @@ def call():
     period = shared_attributes.get('periodSendTelemetry', default_data.periodSendTelemetry)
     while True:
         if CLIENT.is_connected():
-            telemetry = format_telemetry(replica_telemetry())
+            telemetry = format_telemetry(telemetries)
             for key, value in telemetry.items():
                 CLIENT.gw_send_telemetry(key, value)
             LOGGER.info('Sent telemetry data')
@@ -16,82 +16,6 @@ def call():
             telemetries.clear()
             telemetries_lock.release()
         time.sleep(period)
-
-
-def replica_telemetry():
-    telemetry = {
-        "fireState": 0,
-        "offOnFire": 0,
-        "smokeState": 0,
-        "floodState": 0,
-        "moveSensor": 0,
-        "miscTemp": 19,
-        "miscHumid": 60,
-        "aircTemp": 35,
-        "aircHumid": 85,
-        "aircAirc1Temp": 30,
-        "aircAirc2Temp": 26,
-        "aircOutdoorTemp": 36,
-        "atsVacP1": 220,
-        "atsVacP2": 10,
-        "atsVacP3": 11,
-        "atsVacFreq": 13,
-        "atsVgenP1": 10,
-        "atsVgenP2": 11,
-        "atsVgenP3": 12,
-        "atsVgenFreq": 17,
-        "atsVloadP1": 20,
-        "atsVloadP2": 21,
-        "atsVloadP3": 22,
-        "atsVloadFreq": 23,
-        "atsIloadP1": 24,
-        "atsIloadP2": 21,
-        "atsIloadP3": 22,
-        "atsGscOil": 11,
-        "atsGscCoolantTemp": 27,
-        "atsGscFuel": 31,
-        "atsGscVbat": 34,
-        "atsGscSpeed": 32,
-        "atsGscPowerTotal": 125,
-        "atsGscPower1": 45,
-        "atsGscPower2": 40,
-        "atsGscPower3": 40,
-        "atsGscKvaTotal": 40,
-        "atsGscKva1": 10,
-        "atsGscKva2": 20,
-        "atsGscKva3": 10,
-        "atsGscRunHoursCounter": 60,
-        "atsGscCrankCounter": 10,
-        "atsCommState": 1,
-        "atsMode": 0,
-        "atsContactorState": 1,
-        "atuAtu1X": 10,
-        "atuAtu1Y": 20,
-        "atuAtu1Z": 30,
-        "atuAtu2X": 10,
-        "atuAtu2Y": 20,
-        "atuAtu2Z": 30,
-        "atuAtu3X": 10,
-        "atuAtu3Y": 20,
-        "atuAtu3Z": 30,
-        "dcVdc": 21,
-        "dcIbat1": 22,
-        "dcBat1Temp": 23,
-        "dcVbat1Div2": 30,
-        "dcIbat2": 12,
-        "dcBat2Temp": 13,
-        "dcVbat2Div2": 14,
-        "dcIbat3": 15,
-        "dcBat3Temp": 16,
-        "dcVbat3Div2": 17,
-        "dcIbat4": 18,
-        "dcBat4Temp": 19,
-        "dcVbat4Div2": 20,
-        "crmuDoorState": 1
-
-    }
-
-    return telemetry
 
 
 def format_telemetry(dict_telemetry):
